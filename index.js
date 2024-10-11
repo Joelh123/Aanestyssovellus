04da2cb23770fa2869ff6f9386b11690d6d91258
Joel H. <143587522+Joelh123@users.noreply.github.com>
2024-10-11 11:31:10 +0300

    added functionality of logging in

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let users = []
+const users = []
 let regex =  /^[a-öA-Ö]+$/
 
 function register() {
@@ -8,6 +8,7 @@ function register() {
     let username = document.getElementById("register-account-name").value
     let password = document.getElementById("register-password").value
 
+    // check if username has characters other than the alphabet
     if (! regex.test(username)) {
         document.getElementById("error-message-register").innerHTML = "Tunnuksessa tulee olla vain kirjaimia"
         return;
@@ -39,8 +40,33 @@ function register() {
 }
 
 function logIn() {
+
+    let currentAddress = document.location.href.substring(0, (document.location.href.length - 10))
+
     let username = document.getElementById("log-in-account-name").value
     let password = document.getElementById("log-in-password").value
 
-    
+    let accountType = document.querySelector('input[name="account-type"]:checked').value;
+
+    if (username == "" || password == "") {
+        document.getElementById("error-message-log-in").innerHTML = "Kaikki kentät tulee täyttää"
+        return;
+    } else {
+        document.getElementById("error-message-log-in").innerHTML = ""
+    }
+
+    for (let user of users) {
+        if (username === user["username"] && password === user["password"]) {
+            if (accountType == "basic") {
+                document.location.href = `${currentAddress}basicapp.html`
+                return;
+            }
+            if (accountType == "admin") {
+                document.location.href = `${currentAddress}adminapp.html`
+                return;
+            }
+        }
+    }
+
+    document.getElementById("error-message-log-in").innerHTML = "Käyttäjää ei löydy"
 }
